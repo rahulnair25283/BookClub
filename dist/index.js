abb69b6431cf9b21aead049b86de8067fe232e5c
Rahul Nair <rahul.nair@aginicx.com>
2018-03-03 20:37:29 +1000

    added /dist to gitignore

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
-console.ignoredYellowBox = ["Remote debugger"];
-AppRegistry.registerComponent('BookClub', () => App);
-//# sourceMappingURL=index.js.map
