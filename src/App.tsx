5f5dfeb0c51b107e02589dff08019b7017884e1f
Rahul Nair <rahul.nair@aginicx.com>
2018-03-06 21:38:16 +1000

    inital book club creation screen

--- src/App.tsx
+++ src/App.tsx
@@ -9,29 +9,63 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableOpacity,
+  TextInput
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+interface Props { };
+interface State {
+  onAdd: boolean;
+  name: string;
+}
 
-export default class App extends Component<{}> {
+export default class App extends Component<Props, State> {
+
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      onAdd: false,
+      name: "",
+    }
+  }
+
+  onCreate = () => {
+    this.setState({
+      onAdd: true,
+    });
+  }
+
+  create = () => {
+    console.log("new book club ", this.state.name, " created")
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to Book Club!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        {this.state.onAdd
+          ?
+          <View style={{ justifyContent: "center", alignItems: "center" }}>
+            <TextInput
+              placeholder={"book club name"}
+              value={this.state.name}
+              onChangeText={name => this.setState({ name })}
+              autoFocus
+              autoCorrect={false}
+              style={styles.nameInput} />
+            <TouchableOpacity style={styles.button} onPress={() => this.create()}>
+              <Text>Save</Text>
+            </TouchableOpacity>
+          </View>
+          :
+          <View>
+            <Text style={styles.welcome}>
+              Welcome!
+            </Text>
+            <TouchableOpacity style={styles.button} onPress={() => this.onCreate()}>
+              <Text>Create</Text>
+            </TouchableOpacity>
+          </View>}
       </View>
     );
   }
@@ -40,9 +74,10 @@ export default class App extends Component<{}> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    top: "30%",
+    justifyContent: 'flex-start',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#fcfcfc',
   },
   welcome: {
     fontSize: 20,
@@ -54,4 +89,17 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+  button: {
+    width: 100,
+    borderWidth: 1,
+    borderColor: "#333333",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  nameInput: {
+    width: 200,
+    borderBottomWidth: 1,
+    borderColor: "#333333",
+    marginBottom: 5,
+  }
 });
